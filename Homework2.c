39e26c4a07a1de0eedc26bad2ca53d81bd246983
Austin Chirgwin <achirgwi@iu.edu>
2019-09-24 19:23:29 -0400

    Add files via upload

---
+++ Homework2.c
@@ -0,0 +1,50 @@
+#pragma warning(disable:4996)    
+#include <stdio.h> 
+#include <math.h>
+
+//Assignment 2
+//Austin Chirgwin
+//ENGR 19700
+//Wednesday, September 8, 2019
+//This program calculates the range of a pumpkin that is launched from ground level based solely on initial velocity and angle of the launch
+
+int main(void)
+{
+	double v;
+	double g = 9.8;
+	double angle;
+	double range;
+
+	printf("This program calculates range (meters) of a pumpkin thrown from the\n");
+	printf("launch velocity (meters per second) and angle of the launch (degrees) from the ground level\n");
+	printf("given that there is zero air resistance.\n\n");
+	
+	//Input initial velocity
+	printf("Enter the initial velocity of the pumpkin (m/s):");
+	scanf("%lf", &v);
+
+	//Input launch angle
+	printf("\nEnter the launch angle of the pumpkin (degrees):");
+	scanf("%lf", &angle);
+	
+	//Conversion from degrees to radians (https://www.rapidtables.com/convert/number/degrees-to-radians.html)
+	angle = angle * 0.01745329252;
+	
+	//Value of the angle doubled
+	angle = angle * 2;
+	
+	//Sine of the angle
+	angle = sin(angle);
+	
+	//Velocity squared
+	v = v * v;
+	
+	//Range of the pumpkin
+	range = v / g;
+	range = range * angle;
+	
+	printf("\nThe range of the pumpkin in meters is: %.2lf\n", range);
+
+return 0;
+
+}
